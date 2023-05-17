659a3f5f34a9e907385605e31a19a6cbd8dccf44
jeving218 <jevingtrapsiya000@gmail.com>
2023-05-17 14:51:57 +0530

    n

--- array.js
+++ array.js
@@ -16,7 +16,7 @@ namearr.forEach((element) => {
 
 console.log(namearr);
 
-const namewithn = namearr.filter((h)=>{
+const namewithn = namearr.filter((h) => {
     return h.endsWith("g")
 })
 
@@ -25,7 +25,56 @@ console.log(namewithn);
 
 // CartBill
 
-cart = [100,21,90]
+cart = [100, 21, 90]
 
-const sumoOfCart = cart.reduce((pre,curr)=> pre + curr, 0)
+const sumoOfCart = cart.reduce((pre, curr) => pre + curr, 0)
 console.log(sumoOfCart);
+
+
+//Check all the number of list is numbers
+
+score = [100, 21, 90, 1121, 200, 300, 400, 500, 600, 700]
+const checkScore = score.every((v) => typeof v === 'number')
+
+console.log(checkScore);
+
+// Check vale of score greater than the number
+
+const checkScoreAbove200 = score.find((v) => v > 200)
+
+console.log(checkScoreAbove200);
+
+
+// Use of the regular expression
+
+let pettern = 'pw'
+let regOne = new RegExp(pettern)
+
+let flag = 'gi'
+let regTwo = new RegExp(pettern, flag)
+
+let regThree = /pw/gi
+
+const strToCheck = "PW is growing at a rapid speed and recently they are working on Pwskills to create skills based pwcontent"
+
+console.log("Regular Expression One:",regOne.test(strToCheck));
+
+console.log("Regular Expression Two:",regTwo.test(strToCheck));
+
+console.log("Regular Expression Three:",regThree.test(strToCheck));
+
+// another result
+
+const  anotherResult = strToCheck.match(regThree);
+console.log(anotherResult);
+
+// replace the the word
+
+const replaceWord = strToCheck.replace(regThree, "P-W");
+console.log(replaceWord);
+
+// find pattern and replace it
+
+const webUrl = "http://jeving%20trapsiya"
+const replacePattern = webUrl.replace(/%\d0/,'-')
+console.log(replacePattern)
