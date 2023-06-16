841c99e6d86a3d8c6b0c181ee09c4741b556e01d
jeving218 <jevingtrapsiya000@gmail.com>
2023-06-16 18:45:43 +0530

    new

--- node/1.js
+++ node/1.js
@@ -1,5 +1,25 @@
-const path = require('path');
+// /// // /// First Code
 
-const filepath = '../1.html';
+const fs = require('fs');
 
-console.log("path", path.sep);
+// console.log('Start Reading:');
+
+// fs.readFile('node/index-1.txt', function(err, data) {
+//     if(err) {
+//         console.log('Error',err);
+//     }
+//     console.log('Data: ', data.toString());
+//     console.log('Done Reading:');
+// });
+
+// console.log('Other Stuff');
+
+
+// /// // /// Second Code
+
+
+
+console.log("------------------------------  Start Reading --------------------------------------");
+const data = fs.readFileSync('node/index-2.txt');
+console.log("Data: " , data.toString());
+console.log("------------------------------  End Reading --------------------------------------");
